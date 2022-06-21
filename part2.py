768e5757bddc676ea0879bdf29a15563ee220d52
Ubuntu <ubuntu@ip-172-31-17-16.eu-west-1.compute.internal>
2022-06-21 08:45:36 +0000

    Ajout notre projet MIJ

--- part2.py
+++ part2.py
@@ -1,55 +1,49 @@
 #pip3 install fastapi uvicorn
+#pip install passlib[bcrypt]
 #uvicorn part2:api --reload
+#http://127.0.0.1:8000/docs ou http://localhost:8000/docs
 
-from fastapi import FastAPI
-from fastapi import Header
-from pydantic import BaseModel
-from fastapi import HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
 
 api = FastAPI(
     title="Fraud detection API",
     description="This API interrogates various maching learning models.You can access performances of the algorithms on the test sets.",
     version="1.0.0")
 
-@api.get('/welcome')
-def welcome_here():
-    """Returns greetings
-    """
-    return "Welcome"
+security = HTTPBasic()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-users_db = [
-    {
-        'user_name': 'Alice',
-        'user_password': 'wonderland'
+users = {
+
+    "Alice": {
+        "username": "Alice",
+        "hashed_password": pwd_context.hash('wonderland'),
     },
-    {
-        'user_name': 'Clementine',
-        'user_password': 'mandarine'
+
+    "Clementine" : {
+        "username" :  "Clementine",
+        "hashed_password" : pwd_context.hash('mandarine'),
     },
-    {
-        'user_name': 'Bob',
-        'user_password': 'builder'
+
+    "Bob" : {
+        "username" :  "Bob",
+        "hashed_password" : pwd_context.hash('builder'),
     }
-]
 
-class Authorization(BaseModel):
-    name:str
-    password:str
+}
 
-@api.post('/name/{name:str}/password/{password:str}')
-def authorization_users(user: Authorization, name, password):
-    try:
-        identif_user_name = list(
-            filter(lambda x: x.get('user_name') == name and x.get('user_password') == password, users_db)
-            )[0]
-        if identif_user_name['user_name']== name and identif_user_name['user_password']== password:
-            return "Hello. You can start to explore models."
-    except IndexError:
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    if not(users.get(username)) or not(pwd_context.verify(credentials.password, users[username]['hashed_password'])):
         raise HTTPException(
-            status_code=404,
-            detail='Unknown username or password')
-    except ValueError:
-        raise HTTPException(
-            status_code=400,
-            detail='Bad Type'
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
         )
+    return credentials.username
+
+@api.get("/user")
+def current_user(username: str = Depends(get_current_user)):
+    return "Hello {}".format(username)
