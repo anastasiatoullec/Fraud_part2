2a791474db0861f2b5309181427e435b74bd415c
Ubuntu <ubuntu@ip-172-31-17-16.eu-west-1.compute.internal>
2022-06-25 19:36:23 +0000

    Ajout new directory

--- api.py
+++ api.py
@@ -8,8 +8,8 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from passlib.context import CryptContext
 from pydantic import BaseModel
-import fraud as fr
 import joblib
+import pandas as pd
 
 
 api = FastAPI(
@@ -22,8 +22,8 @@ api = FastAPI(
     },
     {   'name': 'Logistic Regression' },
     {   'name': 'Support vector machines'},
-    {   'name': 'K Nearest Neighbors Classifier'},
     {   'name': 'Decision tree'},
+    {   'name': 'K Nearest Neighbors Classifier'},
 ])
 
 security = HTTPBasic()
@@ -68,10 +68,7 @@ def current_user(username: str = Depends(get_current_user)):
 def perfomances():
     """Returns perfomances of the model Logistic Regression
     """
-    loaded_lr= joblib.load('sav/lr_model.sav')
-    X_train, y_train, X_test, y_test= fr.preparing_data()
-    pred = loaded_lr.predict(X_test)
-    log_eval =  fr.evaluate_model(y_test, pred)
+    log_eval = joblib.load('./log_eval.pkl')
     return {"Accuracy, %": (log_eval['acc']*100).round(2),
            "Precision, %": (log_eval['prec']*100).round(2),
            "Recall, %": (log_eval['rec']*100).round(2),
@@ -83,10 +80,7 @@ def perfomances():
 def perfomances():
     """Returns perfomances of the model support vector machines
     """
-    loaded_svm= joblib.load('sav/svm_model.sav')
-    X_train, y_train, X_test, y_test= fr.preparing_data()
-    pred = loaded_svm.predict(X_test)
-    svm_eval =  fr.evaluate_model(y_test, pred)
+    svm_eval = joblib.load('./svm_eval.pkl')
     return {"Accuracy, %": (svm_eval['acc']*100).round(2),
            "Precision, %": (svm_eval['prec']*100).round(2),
            "Recall, %": (svm_eval['rec']*100).round(2),
@@ -94,21 +88,16 @@ def perfomances():
            "Balanced accuracy, %": (svm_eval['b']*100).round(2)
         }
 
-
-
 @api.get("/tree", tags=['Decision tree'])
 def perfomances():
     """Returns perfomances of the model Decision tree
     """
-    loaded_tree = joblib.load('sav/tree_model.sav')
-    X_train, y_train, X_test, y_test= fr.preparing_data()
-    pred = loaded_tree.predict(X_test)
-    tree_eval =  fr.evaluate_model(y_test, pred)
-    return {"Accuracy, %": (tree_eval['acc']*100).round(2),
-           "Precision, %": (tree_eval['prec']*100).round(2),
-           "Recall, %": (tree_eval['rec']*100).round(2),
-           "F1 Score, %": (tree_eval['f1']*100).round(2),
-           "Balanced accuracy, %": (tree_eval['b']*100).round(2)
+    dtc_eval = joblib.load('./dtc_eval.pkl')
+    return {"Accuracy, %": (dtc_eval['acc']*100).round(2),
+           "Precision, %": (dtc_eval['prec']*100).round(2),
+           "Recall, %": (dtc_eval['rec']*100).round(2),
+           "F1 Score, %": (dtc_eval['f1']*100).round(2),
+           "Balanced accuracy, %": (dtc_eval['b']*100).round(2)
         }
 
 
@@ -116,12 +105,7 @@ def perfomances():
 def perfomances():
     """Returns perfomances of the model K Nearest Neighbors Classifier
     """
-
-    loaded_knn = joblib.load('sav/knn_model.sav')
-    X_train, y_train, X_test, y_test= fr.preparing_data()
-    pred = loaded_knn.predict(X_test)
-    knc_eval =  fr.evaluate_model(y_test, pred)
-
+    knc_eval = joblib.load('./knc_eval.pkl')
     return {"Accuracy, %": (knc_eval['acc']*100).round(2),
            "Precision, %": (knc_eval['prec']*100).round(2),
            "Recall, %": (knc_eval['rec']*100).round(2),
@@ -141,19 +125,10 @@ class FraudDetection(BaseModel):
     purchase_month: int
     purchase_year: int
     purchase_value: float
+    source: str
+    browser: str
+    sex: str
     age: int
-    source_Ads: int
-    source_Direct: int
-    source_SEO: int
-    browser_Chrome: int
-    browser_FireFox: int
-    browser_IE: int
-    browser_Opera: int
-    browser_Safari: int
-    sex_F: int
-    sex_M: int
-
-
 
 @api.post("/predict1",tags=['Logistic Regression'])
 def predictions1(fraud:FraudDetection):
@@ -170,24 +145,18 @@ def predictions1(fraud:FraudDetection):
     fraud.purchase_month,
     fraud.purchase_year,
     fraud.purchase_value,
-    fraud.age,
-    fraud.source_Ads,
-    fraud.source_Direct,
-    fraud.source_SEO,
-    fraud.browser_Chrome,
-    fraud.browser_FireFox,
-    fraud.browser_IE,
-    fraud.browser_Opera,
-    fraud.browser_Safari,
-    fraud.sex_F,
-    fraud.sex_M
+    fraud.source,
+    fraud.browser,
+    fraud.sex,
+    fraud.age
     ]]
-    loaded_lr = joblib.load('sav/knn_model.sav')
-    prediction = loaded_lr.predict(features).tolist()[0]
+    log_model = joblib.load('./log_model.pkl')
+    new_case = (pd.DataFrame(features, index = ['0'], columns = ['user_id','signup_day', 'signup_month', 'signup_year', 
+        'purchase_day', 'purchase_month', 'purchase_year','purchase_value', 'source','browser','sex','age']))     
+    prediction = log_model.predict(new_case)
     return {
-        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction
+        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction.tolist()
     }
-
 
 @api.post("/predict2",tags=['Support vector machines'])
 def predictions2(fraud:FraudDetection):
@@ -204,61 +173,20 @@ def predictions2(fraud:FraudDetection):
     fraud.purchase_month,
     fraud.purchase_year,
     fraud.purchase_value,
-    fraud.age,
-    fraud.source_Ads,
-    fraud.source_Direct,
-    fraud.source_SEO,
-    fraud.browser_Chrome,
-    fraud.browser_FireFox,
-    fraud.browser_IE,
-    fraud.browser_Opera,
-    fraud.browser_Safari,
-    fraud.sex_F,
-    fraud.sex_M
+    fraud.source,
+    fraud.browser,
+    fraud.sex,
+    fraud.age
     ]]
-    loaded_svm = joblib.load('sav/svm_model.sav')
-    prediction = loaded_svm.predict(features).tolist()[0]
+    svm_model = joblib.load('./svm_model.pkl')
+    new_case = (pd.DataFrame(features, index = ['0'], columns = ['user_id','signup_day', 'signup_month', 'signup_year', 
+        'purchase_day', 'purchase_month', 'purchase_year','purchase_value', 'source','browser','sex','age']))     
+    prediction = svm_model.predict(new_case)
     return {
-        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction
+        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction.tolist()
     }
 
-
-@api.post("/predict4",tags=['Decision tree'])
-def predictions4(fraud:FraudDetection):
-    """
-    :param:input data from the post request
-    :return predicted type
-    """
-    features = [[
-    fraud.user_id,
-    fraud.signup_day,
-    fraud.signup_month,
-    fraud.signup_year,
-    fraud.purchase_day,
-    fraud.purchase_month,
-    fraud.purchase_year,
-    fraud.purchase_value,
-    fraud.age,
-    fraud.source_Ads,
-    fraud.source_Direct,
-    fraud.source_SEO,
-    fraud.browser_Chrome,
-    fraud.browser_FireFox,
-    fraud.browser_IE,
-    fraud.browser_Opera,
-    fraud.browser_Safari,
-    fraud.sex_F,
-    fraud.sex_M
-    ]]
-    loaded_tree = joblib.load('sav/tree_model.sav')
-    prediction = loaded_tree.predict(features).tolist()[0]
-    return {
-        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction
-    }
-
-
-
-@api.post("/predict3",tags=['K Nearest Neighbors Classifier'])
+@api.post("/predict3",tags=['Decision tree'])
 def predictions3(fraud:FraudDetection):
     """
     :param:input data from the post request
@@ -273,23 +201,43 @@ def predictions3(fraud:FraudDetection):
     fraud.purchase_month,
     fraud.purchase_year,
     fraud.purchase_value,
-    fraud.age,
-    fraud.source_Ads,
-    fraud.source_Direct,
-    fraud.source_SEO,
-    fraud.browser_Chrome,
-    fraud.browser_FireFox,
-    fraud.browser_IE,
-    fraud.browser_Opera,
-    fraud.browser_Safari,
-    fraud.sex_F,
-    fraud.sex_M
+    fraud.source,
+    fraud.browser,
+    fraud.sex,
+    fraud.age
     ]]
-    loaded_knn = joblib.load('sav/knn_model.sav')
-    prediction = loaded_knn.predict(features).tolist()[0]
+    dtc_model = joblib.load('./dtc_model.pkl')
+    new_case = (pd.DataFrame(features, index = ['0'], columns = ['user_id','signup_day', 'signup_month', 'signup_year', 
+        'purchase_day', 'purchase_month', 'purchase_year','purchase_value', 'source','browser','sex','age']))     
+    prediction = dtc_model.predict(new_case)
     return {
-        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction
+        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction.tolist()
     }
 
-
-
+@api.post("/predict4",tags=['K Nearest Neighbors Classifier'])
+def predictions4(fraud:FraudDetection):
+    """
+    :param:input data from the post request
+    :return predicted type
+    """
+    features = [[
+    fraud.user_id,
+    fraud.signup_day,
+    fraud.signup_month,
+    fraud.signup_year,
+    fraud.purchase_day,
+    fraud.purchase_month,
+    fraud.purchase_year,
+    fraud.purchase_value,
+    fraud.source,
+    fraud.browser,
+    fraud.sex,
+    fraud.age
+    ]]
+    knc_model = joblib.load('./knc_model.pkl')
+    new_case = (pd.DataFrame(features, index = ['0'], columns = ['user_id','signup_day', 'signup_month', 'signup_year', 
+        'purchase_day', 'purchase_month', 'purchase_year','purchase_value', 'source','browser','sex','age']))     
+    prediction = knc_model.predict(new_case)
+    return {
+        "Predicted transaction(1 - fraud, 0 - not fraud)":prediction.tolist()
+    }
---
+++ docker_test2/tests_not_fraud.py
@@ -0,0 +1,130 @@
+import os
+import requests
+
+# définition de l'adresse de l'API
+api_address = '127.0.0.1'
+# port de l'API
+api_port = 8000
+
+url1='http://{address}:{port}/predict1'.format(address=api_address, port=api_port)
+
+data= {  
+  "user_id": 22058,
+  "signup_day": 24,
+  "signup_month": 2,
+  "signup_year": 2015,
+  "purchase_day": 18,
+  "purchase_month": 4,
+  "purchase_year": 2015,
+  "purchase_value": 34,
+  "source": "SEO",
+  "browser": "Chrome",
+  "sex": "M",
+  "age": 39
+}
+
+r = requests.post(url1, json=data)
+
+output = '''
+========================================================
+    Not fraud prediction test for logistic regression
+========================================================
+
+request done at "/predict1"
+
+expected result = 200
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+url2='http://{address}:{port}/predict2'.format(address=api_address, port=api_port)
+
+r = requests.post(url2, json=data)
+
+output = '''
+=============================================================
+    Not fraud prediction test for support vector machines
+=============================================================
+
+request done at "/predict2"
+
+expected result = 200
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+url3='http://{address}:{port}/predict3'.format(address=api_address, port=api_port)
+
+r = requests.post(url3, json=data)
+
+output = '''
+=============================================================
+    Not fraud prediction test for decision tree classifier
+=============================================================
+
+request done at "/predict3"
+
+expected result = 200
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+url4='http://{address}:{port}/predict4'.format(address=api_address, port=api_port)
+
+r = requests.post(url4, json=data)
+
+output = '''
+==================================================================
+    Not fraud prediction test for K Nearest Neighbors Classifier
+==================================================================
+
+request done at "/predict4"
+
+expected result = 200
+actual result = {status_code}
+
+==>  {test_status}
+
+'''
+# statut de la requête
+status_code = r.status_code
+
+# affichage des résultats
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
